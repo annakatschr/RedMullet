e6af5587c71d75c597303d793526250282b8c0e5
annakatschr <annakatschr@gmail.com>
2026-03-05 23:00:46 +0100

    corrections

--- VirusTotalAPI.py
+++ VirusTotalAPI.py
@@ -1,12 +1,15 @@
 import json
+from pickle import NONE
 import re
+from unittest import result
 from urllib import response
-#import mailparser
-#import requests
-#from urlextract import URLExtract
+import mailparser
+import requests
+from urlextract import URLExtract
 import hashlib
 from dotenv import load_dotenv
 import os
+import ipaddress
 
 class VirusTotalAPI():
     def __init__(self): # constructor for the VirusTotalAPI class
@@ -31,7 +34,6 @@ class VirusTotalAPI():
 
         return sha256.hexdigest()
 
-
     def md5_file(self, attachment):
     
         with open(attachment, "rb") as f:
@@ -53,12 +55,15 @@ class VirusTotalAPI():
             mail_parsed = mailparser.parse_from_bytes(f.read())
 
         self.parsed_mail = mail_parsed
-      #  return mail_parsed
+        
     
     def send_request(self, url, header, payload=None):
         try:
-            response = requests.get(url, headers=header, payload=payload)
-            response.raise_for_status()  # Raise an exception for HTTP errors
+            if payload == None:
+                response = requests.get(url, headers=header)                
+            else:
+                response = requests.get(url, headers=header, payload=payload)         
+            response.raise_for_status() # Raise an exception for HTTP errors
             return response.json()  # Return the JSON response if successful
         except requests.RequestException as e:
             print(f"Error fetching data from VirusTotal API: {e}")
@@ -67,19 +72,15 @@ class VirusTotalAPI():
     
     def check_sender_domain(self):
         sender = self.parsed_mail.from_
-        print(sender)
-
         url = self.base + "domains/" + sender[0][1].split("@")[1]
-        print(url)
-
         response = self.send_request(url, self.headers)
-    
-        #    response = requests.get(url, headers=self.headers)
-  
-        print(response.text)
-        print(json.dumps(response, indent=4, sort_keys=True, ensure_ascii=False))
+ 
+        print(response["data"]["attributes"]["last_analysis_stats"])
+        results = response["data"]["attributes"]["last_analysis_stats"]
+        if results["malicious"] > 0 or results["suspicious"] > 0:
+            print(f"The sender's domain({sender[0][1].split('@')[1]}) is flagged as malicious or suspicious.")
 
-
+       # print(json.dumps(response, indent=4, sort_keys=True, ensure_ascii=False))
 
     def check_mail_attachments(self, attachments):
 
@@ -100,34 +101,40 @@ class VirusTotalAPI():
     def check_header_ips(self):
         ips = re.findall(r"\b\d{1,3}(?:\.\d{1,3}){3}\b", str(self.parsed_mail.headers))
         ips = self.remove_duplicates(ips)
-        # check that the addresses recoved are a ctualy valid ip address and not just a random string of numbers
-        print(ips)
+       
+        for ip in ips:
+            try:
+                ipaddress.ip_address(ip)
+                url = self.base + "ip_addresses/" + ip
+                #response = requests.get(url, headers=self.headers)
+                response = self.send_request(url, self.headers)
+                print(response["data"]["attributes"]["last_analysis_stats"])
+                if response["data"]["attributes"]["last_analysis_stats"]["malicious"] > 0 or response["data"]["attributes"]["last_analysis_stats"]["suspicious"] > 0:
+                    print(f"The IP address {ip} is flagged as malicious or suspicious.")
+               # print(response.text)
+            except ValueError:
+                print(f"{ip} is not a valid IP address.")
+                #ips.remove(ip)
+     
 
-        url = self.base + "ip_addresses/" + ips[0] if ips else "invalid_ip"
-        response = requests.get(url, headers=self.headers)
-
-        print(response.text)
-
-
-    def check_mail_urls(self, url):
-        urls_concentrated = ''
-        extractor = URLExtract()
-
-        for url in extractor.gen_urls(self.parsed_mail.text_plain):
-            urls_concentrated += url + ", "
+    def check_mail_urls(self):
         
-        if urls_concentrated != '':
-            url = self.base + "urls"
-            
-            payload = { "url": urls_concentrated }
+        extractor = URLExtract()
+     #   print(self.parsed_mail.body)
+        urls = extractor.find_urls(self.parsed_mail.body)
+     
+        for url in urls:
+            print(url)
+            url = self.base + "urls/" + url
+           # payload = { "url": url }
             headers = {
                 "accept": "application/json",
                 "x-apikey": os.getenv("API_KEY"),
-                "content-type": "application/x-www-form-urlencoded"
+               # "content-type": "application/x-www-form-urlencoded"
             }
-
-           # response = requests.get(url, payload = payload, headers=headers)
-            response = self.send_request(url, headers, payload)
+            response = self.send_request(url, headers)
+            if response["data"]["attributes"]["last_analysis_stats"]["malicious"] > 0 or response["data"]["attributes"]["last_analysis_stats"]["suspicious"] > 0:
+                    print(f"The URL {url} is flagged as malicious or suspicious.")
             print(response.text)
         
 
@@ -135,7 +142,7 @@ if __name__ == "__main__":
     VT_mailchecker = VirusTotalAPI()
     msg = VT_mailchecker.parse_email("test_mail2.eml")
     #VT_mailchecker.check_mail_attachments(msg.attachments)
-    VT_mailchecker.check_sender_domain()
+    VT_mailchecker.check_mail_urls()
 
         #  read from file
     #  mail_parsed = mailparser.parse_from_file(mail)
